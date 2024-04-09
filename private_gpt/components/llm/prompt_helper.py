0adfda3d2f1d5523672ac39fc9a9d0774da3f3af
Tuan T. Pham <tuan@vt.edu>
2024-04-09 01:56:05 -0400

    Tidy up get_prompt_style function
    private_gpt/components/llm/prompt_helper.py:
        * Using a map of classes to return prompt style
        * Improved by Mixtral 8x7B suggestion
    
    Signed-off-by: Tuan T. Pham <tuan@vt.edu>

--- private_gpt/components/llm/prompt_helper.py
+++ private_gpt/components/llm/prompt_helper.py
@@ -165,23 +165,17 @@ class ChatMLPromptStyle(AbstractPromptStyle):
             [ChatMessage(content=completion, role=MessageRole.USER)]
         )
 
+PROMPT_STYLE_CLS_MAP = {
+    None: DefaultPromptStyle,
+    "default": DefaultPromptStyle,
+    "llama2": Llama2PromptStyle,
+    "tag": TagPromptStyle,
+    "mistral": MistralPromptStyle,
+    "chatml": ChatMLPromptStyle,
+}
 
-def get_prompt_style(
-    prompt_style: Literal["default", "llama2", "tag", "mistral", "chatml"] | None
-) -> AbstractPromptStyle:
-    """Get the prompt style to use from the given string.
-
-    :param prompt_style: The prompt style to use.
-    :return: The prompt style to use.
-    """
-    if prompt_style is None or prompt_style == "default":
-        return DefaultPromptStyle()
-    elif prompt_style == "llama2":
-        return Llama2PromptStyle()
-    elif prompt_style == "tag":
-        return TagPromptStyle()
-    elif prompt_style == "mistral":
-        return MistralPromptStyle()
-    elif prompt_style == "chatml":
-        return ChatMLPromptStyle()
+def get_prompt_style(prompt_style: str | None) -> AbstractPromptStyle:
+    style_cls = PROMPT_STYLE_CLS_MAP.get(prompt_style)
+    if style_cls:
+        return style_cls()
     raise ValueError(f"Unknown prompt_style='{prompt_style}'")
